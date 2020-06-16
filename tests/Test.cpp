30ab1f42df1284328fa42d8b5052716706868e11
mingfei.deng <mingfei.deng@thoughtworks.com>
2020-06-16 20:56:43 +0800

    should_return_ticket_when_store_bag_given_first_full_and_second_available test pass.

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -100,6 +100,28 @@ TEST(robot, should_return_ticket_when_store_bag_given_both_lockers_are_available
   EXPECT_EQ(19, locker2.remain);
 }
 
+TEST(robot, should_return_ticket_when_store_bag_given_first_full_and_second_available){
+  //given
+  Locker locker1(1);
+  Locker locker2(19);
+  std::vector<Locker*> robot_manage_lockers;
+  robot_manage_lockers.push_back(&locker1);
+  robot_manage_lockers.push_back(&locker2);
+  Robot robot(robot_manage_lockers);
+  Bag bag1(666);
+  (void)robot.SaveBag(bag1);
+  Bag bag2(6666);
+
+  //when
+  SaveBagResult result = robot.SaveBag(bag2);
+
+  //then
+  EXPECT_EQ(0, result.err);
+  EXPECT_NE(0, result.ticket.id);
+  EXPECT_EQ(0, locker1.remain);
+  EXPECT_EQ(18, locker2.remain);
+}
+
 
 
 
